6fff28a19ba0d7e2f4b9a99c0bf536c6218618e9
Stephen <stephen.lyssy@gmail.com>
2021-02-22 07:23:55 -0600

    Updated tooltip x position to test if it works better with deployed app.

--- static/precipitation.js
+++ static/precipitation.js
@@ -148,7 +148,7 @@ console.log(data)
         d3.select("#annualPrecipitation").text(" "+ d.PRCP + " inches");
 
         //Position the tooltip <div> and set its content
-        let x = event.pageX -1100;
+        let x = event.pageX;
         let y = event.pageY - 1000;
 
         //Position tooltip and make it visible
