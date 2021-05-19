9ba00c8302ad1d07e7da99e6fee5012f9a675e39
Stephen <stephen.lyssy@gmail.com>
2021-05-18 22:01:07 -0500

    Made the weatherStationsTotal chart and annualTotals chart svgs responsive using code I got from https://benclinkinbeard.com/d3tips/make-any-chart-responsive-with-one-function/?utm_content=buffer976d6&utm_medium=social&utm_source=twitter.com&utm_campaign=buffer

--- static/weatherStationTotals.js
+++ static/weatherStationTotals.js
@@ -15,6 +15,43 @@ function chartTrend(csvTrends) {
     return b - a;
   });
   // console.log(weatherStation);
+  function responsivefy(svg) {
+    // container will be the DOM element
+    // that the svg is appended to
+    // we then measure the container
+    // and find its aspect ratio
+    const container = d3.select(svg.node().parentNode),
+      width = parseInt(svg.style("width"), 10),
+      height = parseInt(svg.style("height"), 10),
+      aspect = width / height;
+
+    // set viewBox attribute to the initial size
+    // control scaling with preserveAspectRatio
+    // resize svg on inital page load
+    svg
+      .attr("viewBox", `0 0 ${width} ${height}`)
+      .attr("preserveAspectRatio", "xMinYMid")
+      .call(resize);
+
+    // add a listener so the chart will be resized
+    // when the window resizes
+    // multiple listeners for the same event type
+    // requires a namespace, i.e., 'click.foo'
+    // api docs: https://goo.gl/F3ZCFr
+    d3.select(window).on("resize." + container.attr("id"), resize);
+
+    // this is the code that resizes the chart
+    // it will be called on load
+    // and in response to window resizes
+    // gets the width of the container
+    // and resizes the svg to fill it
+    // while maintaining a consistent aspect ratio
+    function resize() {
+      const w = parseInt(container.style("width"));
+      svg.attr("width", w);
+      svg.attr("height", Math.round(w / aspect));
+    }
+  }
 
   d3.select("#weatherStation")
     .selectAll("option")
@@ -23,8 +60,8 @@ function chartTrend(csvTrends) {
     .append("option")
     .text((d) => d);
 
-  var svg = d3.select("#prcpTrends"),
-    margin = { top: 70, right: -35, bottom: 20, left: 110 },
+  var svg = d3.select("#prcpTrends").call(responsivefy),
+    margin = { top: 70, right: 50, bottom: 20, left: 110 },
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom;
   // .append("div")
@@ -32,7 +69,6 @@ function chartTrend(csvTrends) {
   // .append("svg")
   // .attr("preserveAspectRatio", "xMinYMin meet")
   // .attr("viewBox", "0 0 1200 548")
-  // .classed("prcpTrends", true)
   // .attr("width", 1200)
   // .attr("height", 548);
 
