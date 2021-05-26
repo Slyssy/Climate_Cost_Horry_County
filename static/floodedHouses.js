a9d7316172fe26e11d49763262909a9d503a6b83
Stephen <stephen.lyssy@gmail.com>
2021-05-25 22:55:45 -0500

    Cleanded up the code so it was more consistent with ES6.

--- static/floodedHouses.js
+++ static/floodedHouses.js
@@ -1,14 +1,14 @@
 // * Load the Data
 d3.csv("/static/Flood_Area_Count.csv").then(function (data) {
   // * Set Demensions and margins for the bar chart
-  var width = 1080;
+  const width = 1080;
   height = 750;
   margin = { left: 100, top: 70, right: 30, bottom: 85 };
 
   // * Setting the ranges
-  var x3 = d3.scaleBand().range([0, width]).padding(0.1);
-  var y3 = d3.scaleLinear().range([height, 0]);
-  var colorScale = d3.scaleOrdinal().range(["#6aeb5e", "#ebe028", "#976aeb"]);
+  const xAxis = d3.scaleBand().range([0, width]).padding(0.1);
+  const yAxis = d3.scaleLinear().range([height, 0]);
+  const colorScale = d3.scaleOrdinal().range(["#6aeb5e", "#ebe028", "#976aeb"]);
 
   function responsivefy(svg) {
     // container will be the DOM element
@@ -51,7 +51,7 @@ d3.csv("/static/Flood_Area_Count.csv").then(function (data) {
   // * append the svg object to the body of the page
   // * append a 'group' element to 'svg'
   // * moves the 'group' element to the top left margin
-  var svg = d3
+  const svg = d3
     .select("#barChart")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -60,16 +60,16 @@ d3.csv("/static/Flood_Area_Count.csv").then(function (data) {
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var g = svg
+  const g = svg
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  var xAxisG = g
+  const xAxisG = g
     .append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + height + ")");
 
-  var yAxisG = g.append("g").attr("class", "y axis");
+  const yAxisG = g.append("g").attr("class", "y axis");
 
   yAxisG
     .append("text")
@@ -93,60 +93,43 @@ d3.csv("/static/Flood_Area_Count.csv").then(function (data) {
     .attr("class", "title")
     .text("Flooded Houses in Each Flood Zone");
   // * Format the data
-  data.forEach(function (d) {
+  data.forEach((d) => {
     d.Flood_Area = d.Flood_Area;
     d.Count = +d.Count;
   });
   // console.log(data)
 
   // * Scale the range of the data in the domains
-  x3.domain(
-    data.map(function (d) {
-      return d.Flood_Area;
-    })
-  );
-  y3.domain([
-    0,
-    d3.max(data, function (d) {
-      return d.Count;
-    }) + 2,
-  ]);
+  xAxis.domain(data.map((d) => d.Flood_Area));
+  yAxis.domain([0, d3.max(data, (d) => d.Count) + 2]);
 
   // * append the rectangles for the bar chart
-  var bar = svg.selectAll(".bar").data(data);
+  const bar = svg.selectAll(".bar").data(data);
 
-  var bar1 = bar
+  const bar1 = bar
     .enter()
     .append("rect")
     .attr("class", "bar")
-    .attr("x", function (d) {
-      return x3(d.Flood_Area);
-    })
-    .attr("width", x3.bandwidth())
-    .attr("y", function (d) {
-      return y3(d.Count);
-    })
-    .attr("height", function (d) {
-      return height - y3(d.Count);
-    })
-    .attr("fill", function (d) {
-      return colorScale(d.Flood_Area);
-    });
+    .attr("x", (d) => xAxis(d.Flood_Area))
+    .attr("width", xAxis.bandwidth())
+    .attr("y", (d) => yAxis(d.Count))
+    .attr("height", (d) => height - yAxis(d.Count))
+    .attr("fill", (d) => colorScale(d.Flood_Area));
 
   // * add the x Axis
   svg
     .append("g")
     .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x3).tickSizeOuter(0))
+    .call(d3.axisBottom(xAxis).tickSizeOuter(0))
     .selectAll("text")
     .attr("class", "floodedHouses_xAxis_tickLabel");
 
   // * add the y Axis
-  svg.append("g").call(d3.axisLeft(y3).ticks(5).tickSize(-width));
+  svg.append("g").call(d3.axisLeft(yAxis).ticks(5).tickSize(-width));
 
   // * Adding Tooltip Behavior
   bar1
-    .on("mouseover", function (event, d) {
+    .on("mouseover", (event, d) => {
       d3.select(this).style("fill", "#ce42f5");
       d3.select("#tool_tip").text(" " + d.Count);
 
@@ -162,9 +145,7 @@ d3.csv("/static/Flood_Area_Count.csv").then(function (data) {
     })
 
     .on("mouseout", function () {
-      d3.select(this).style("fill", function (d) {
-        return colorScale(d.Flood_Area);
-      });
+      d3.select(this).style("fill", (d) => colorScale(d.Flood_Area));
 
       //* Hide the tooltip
       d3.select("#tooltip-bar").style("opacity", "0");
